110909907af4d0d6d868b482f8ebf25d72a92429
ArshathAkbar <140537826+ArshathAkbar@users.noreply.github.com>
2024-02-16 20:11:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
-year = 2024
+#year = 2024
+year = 2023
 if is_leap_year(year):
     print(f"{year} is a leap year.")
 else:
